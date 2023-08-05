72ce08c6b5e1bf782bbfaa42b2e6b57646b8045b
Ical Risaldi <r.harisrisaldi@gmail.com>
2023-08-05 21:58:52 +0700

    Initial Commit

---
+++ qrcodereader/qrcodereaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  qrcodereaderApp.swift
+//  qrcodereader
+//
+//  Created by Rizky Haris Risaldi on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct qrcodereaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
